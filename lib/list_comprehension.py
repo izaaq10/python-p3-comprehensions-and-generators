b101c4596b9af767dc3a492ed6707ee09bc63947
Izaaq <odongoizaaq@gmail.com>
2024-05-22 06:02:13 -0400

    commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_lc = [n for n in num_list if n % 2 == 0]
+    return even_lc
+
     pass
 
 def make_exclamation(sentence_list):
-    pass
+   exclamation_szn = [n + "!"for n in sentence_list
+    ]
+   return exclamation_szn
+    
